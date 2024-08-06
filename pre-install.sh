4c84568626d1df51550ec2207b16e1a95dd49db6
yida <yida.niu73@outlook.com>
2024-08-06 15:47:36 +0800

    fixed pre-install failure

--- pre-install.sh
+++ pre-install.sh
@@ -10,10 +10,10 @@ Check if oh-my-zsh is installed
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
   echo 'Installing oh-my-zsh'
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo Y | /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
   echo 'Updating oh-my-zsh'
-  /usr/bin/zsh -c "source $ZSH/oh-my-zsh.sh && omz update"
+  /usr/bin/zsh -c "$OMZDIR/tools/upgrade.sh"
 fi
 
 # Change default shell
